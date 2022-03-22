686305366fa5db7f85de0d29e7904cc641d421bf
JohnTseung <u3578708@connect.hku.hk>
2022-03-22 10:03:31 +0800

    Add filter and launch file; minor correction

--- encoder_driver/src/encoder_node.cpp
+++ encoder_driver/src/encoder_node.cpp
@@ -12,31 +12,40 @@
 
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
-// add the includes needed for socketCAN
-
-int int_pow(int x, int exp) {
-    int result = 1;
-    for (int i = 0; i < exp; i++){
-        result *= x;
-    }
-    return result;
-}
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "encoder_node");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
+    std::string node_name = nh.resolveName("");
 
-    ros::Publisher encoder_pub = nh.advertise<std_msgs::Int32>("p_feedback", 1000);
+    ros::Publisher encoder_pub = nh.advertise<std_msgs::Int32>("p_feedback", 1);
+
+    // param from launch file
+    int can_id;
+    if (nh.getParam("can_id", can_id)) {
+        ROS_INFO("%s: Can id: %i", node_name.c_str(), can_id);
+    } else {
+        ROS_ERROR("%s: Cannot retrieve can id", node_name.c_str());
+        return 1;
+    }
+
+    int device_id;
+    if (nh.getParam("device_id", device_id)) {
+        ROS_INFO("%s: Device id: %i", node_name.c_str(), device_id);
+    } else {
+        ROS_ERROR("%s: Cannot retrieve device id", node_name.c_str());
+        return 1;
+    }
 
     // set up socket
     int s;
     int nbytes;
-    struct sockaddr_can addr;
-    struct ifreq ifr;
-    struct can_frame frame;
+    sockaddr_can addr;
+    ifreq ifr;
+    can_frame frame;
 
     if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-        perror("Socket");
+        ROS_ERROR("Socket");
         return 1;
     }
 
@@ -47,50 +56,56 @@ int main(int argc, char **argv) {
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
 
-    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        perror("Bind");
+    if (bind(s, (sockaddr *)&addr, sizeof(addr)) < 0) {
+        ROS_ERROR("Bind");
         return 1;
     }
 
-    int count = 0;
+    can_filter rfilter[1];
+    rfilter[0].can_id   = can_id;
+    rfilter[0].can_mask = CAN_EFF_MASK; // enable all 29 filter bits
+
+    setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter));
+
     while (ros::ok()) {
         // read the CAN frame
-        nbytes = read(s, &frame, sizeof(struct can_frame));
+        nbytes = read(s, &frame, sizeof(can_frame));
      	if (nbytes < 0) {
-            perror("Read");
-            return 1;
+            ROS_ERROR_THROTTLE(1, "Read");
+            continue;
     	}
+        if (frame.data[1] != device_id) {
+            // device id not match
+            // TODO: how to handle this
+            continue;
+        }
 
         // print data
-        char data_str[32];
-        int idx = 0;
-        ROS_INFO("0x%03X [%d] ", frame.can_id, frame.can_dlc);
-        for (int i = 0; i < frame.can_dlc; i++) {
-            idx += snprintf(data_str+idx, sizeof data_str, "%02X ", frame.data[i]);
-        }
-        ROS_INFO("%s\n", data_str);
+        // char data_str[32];
+        // int idx = 0;
+        // ROS_INFO("0x%03X [%d] ", frame.can_id, frame.can_dlc);
+        // for (int i = 0; i < frame.can_dlc; i++) {
+        //     idx += snprintf(data_str+idx, sizeof data_str, "%02X ", frame.data[i]);
+        // }
+        // ROS_INFO("%s\n", data_str);
 
         // fill in msg based on the contents from CAN frame
         uint8_t len = frame.data[0];
-        if (len-3 < 4){ // len of data only, exclude first 3 ele
-            perror("Not enough data to parse int32");
-            return 1;
+        if (len != frame.can_dlc) {
+            ROS_WARN_THROTTLE(1, "frame size received not match with data len");
+            continue;
         }
-        int hexcount = 6;
+        if (len - 3 < 4) {
+            ROS_WARN_THROTTLE(1, "byte too short, not enough data to parse int32");
+            continue;
+        }
         std_msgs::Int32 msg;
-        msg.data = 0;
-        for (int i = len-1; i >= len-4; i--) {
-            msg.data += frame.data[i] * int_pow(16, hexcount);
-            hexcount -= 2;
-        }
-        // publish msg
+        memcpy(&msg.data, frame.data + 3, 4);
         encoder_pub.publish(msg);
-
-        count++;
     }
     // close socket
     if (close(s) < 0) {
-        perror("Close");
+        ROS_ERROR("Close");
         return 1;
     }
     return 0;
